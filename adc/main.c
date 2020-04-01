3081e763acfb78f32f097e81c9c5838cd8d494d5
Umesh Lokhande <38487819+tinkersky@users.noreply.github.com>
2020-04-01 17:32:32 +0530

    Update main.c

--- adc/main.c
+++ adc/main.c
@@ -10,21 +10,19 @@ void ADC_Init(){
 	ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
 }
 
-uint16_t ADC_Read(uint8_t ch){
-	ADCSRA = 0x8F;   // 10001111
-	ADMUX = 0xE0;    // 11100000
+uint8_t ADC_Read(){ 
 	ADCSRA |= 1<<ADSC;
-
-	return ADCH;
+	return ADC;
 }
 
 int main(void)
 {
 	UART_Init();
+	ADC_Init();
 	
 	while (1)
 	{
-		uint16_t adc_value = ADC_Read(0)*0.4882;
+		uint8_t adc_value = ADC_Read()*0.4882;
 		char val[4];
 		itoa(adc_value,val,10);
 		UART_TxString(val);
